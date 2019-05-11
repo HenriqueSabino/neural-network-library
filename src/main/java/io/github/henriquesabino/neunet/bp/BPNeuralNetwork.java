b17890000004dea32bf7189e43e9ca384d1eeb93
HenriqueSabino <h.sabinocosta@gmail.com>
2019-05-10 22:55:41 -0300

    Finished implementing online training neural network

--- src/main/java/io/github/henriquesabino/neunet/bp/BPNeuralNetwork.java
+++ src/main/java/io/github/henriquesabino/neunet/bp/BPNeuralNetwork.java
@@ -6,6 +6,7 @@ import io.github.henriquesabino.neunet.NeuralNetwork;
 public class BPNeuralNetwork extends NeuralNetwork {
     
     private static final long serialVersionUID = -3732215973838717612L;
+    private Matrix[] costs;
     private double cost, learningRate;
     
     public BPNeuralNetwork(int inputsNum, int[] hiddenLayersSize, int outputsNum, double learningRate) {
@@ -13,23 +14,100 @@ public class BPNeuralNetwork extends NeuralNetwork {
         this.learningRate = learningRate;
         
         costs = new Matrix[netSize];
-        deltaWeights = new Matrix[netSize];
-        deltaBiases = new Matrix[netSize];
         cost = 0;
         
         for (int i = 0; i < neurons.length; i++) {
             costs[i] = neurons[i].copy();
             costs[i].applyForEach(x -> 0.0);
         }
+    }
+    
+    public void train(double[] inputs, double[] expected) {
         
-        for (int i = 0; i < deltaWeights.length; i++) {
-            deltaWeights[i] = weights[i].copy();
-            deltaWeights[i].applyForEach(x -> 0.0);
+        double[] predicted = predict(inputs);
+        
+        calculateCosts(expected, predicted);
+        adjustWeights(inputs);
+        adjustBiases();
+    }
+    
+    private void calculateCosts(double[] expected, double[] predicted) {
+        
+        cost = 0;
+        
+        for (int i = 0; i < expected.length; i++) {
+            double value = -(expected[i] - predicted[i]);
+            cost += Math.pow(value, 2) / 2;
+            costs[netSize - 1].setValue(i, 0, value);
         }
         
-        for (int i = 0; i < deltaBiases.length; i++) {
-            deltaBiases[i] = biases[i].copy();
-            deltaBiases[i].applyForEach(x -> 0.0);
+        //Looping backwards
+        //We have to iterate by the columns first because they determine the neuron
+        //of the previous layer that the weight is applied to
+        for (int l = netSize - 2; l >= 0; l--) {
+            for (int n = 0; n < weights[l + 1].getColumns(); n++) {
+                
+                double value = 0;
+                
+                for (int w = 0; w < weights[l + 1].getRows(); w++) {
+                    value += weights[l + 1].getValue(w, n) * dReLU(sums[l + 1].getValue(w, 0))
+                            * costs[l + 1].getValue(w, 0);
+                }
+                
+                costs[l].setValue(n, 0, value);
+            }
+        }
+    }
+    
+    private void adjustWeights(double[] inputs) {
+        
+        for (int l = 0; l < weights.length; l++) {
+            for (int i = 0; i < weights[l].getRows(); i++) {
+                for (int j = 0; j < weights[l].getColumns(); j++) {
+                    
+                    //If it is the first layer
+                    if (l == 0) {
+                        double value = inputs[j] * dReLU(sums[l].getValue(i, 0)) * costs[l].getValue(i, 0);
+                        value *= -learningRate;
+                        value += weights[l].getValue(i, j);
+                        weights[l].setValue(i, j, value);
+                    } else {
+                        if (l != netSize - 1) {
+                            double value = neurons[l - 1].getValue(j, 0) * dReLU(sums[l].getValue(i, 0)) * costs[l].getValue(i, 0);
+                            value *= -learningRate;
+                            value += weights[l].getValue(i, j);
+                            weights[l].setValue(i, j, value);
+                        } else {
+                            double value = neurons[l - 1].getValue(j, 0) * dSig(sums[l].getValue(i, 0)) * costs[l].getValue(i, 0);
+                            value *= -learningRate;
+                            value += weights[l].getValue(i, j);
+                            weights[l].setValue(i, j, value);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private void adjustBiases() {
+        
+        for (int l = 0; l < biases.length; l++) {
+            for (int n = 0; n < biases[l].getRows(); n++) {
+                
+                if (l != netSize - 1) {
+                    
+                    double value = dReLU(sums[l].getValue(n, 0)) * costs[l].getValue(n, 0);
+                    value *= -learningRate;
+                    value += biases[l].getValue(n, 0);
+                    biases[l].setValue(n, 0, value);
+                    
+                } else {
+                    double value = dSig(sums[l].getValue(n, 0)) * costs[l].getValue(n, 0);
+                    value *= -learningRate;
+                    value += biases[l].getValue(n, 0);
+                    biases[l].setValue(n, 0, value);
+                }
+            }
         }
     }
     
